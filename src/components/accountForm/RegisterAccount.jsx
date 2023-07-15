61f9963da065bdfad802e99e9773ceb11f2b2fe9
Samuel Tetteh <khobbylynx55@gmail.com>
2023-07-15 03:35:45 +0000

    Update RegisterAccount.jsx

--- src/components/accountForm/RegisterAccount.jsx
+++ src/components/accountForm/RegisterAccount.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaApple } from 'react-icons/fa'
 import Modal from '../portal/Modal'
@@ -6,25 +6,40 @@ import './GeneralFormStyle.scss'
 import { Link } from 'react-router-dom'
 import newRequest from '../../utils/newRequest'
 
-const RegisterAccount = ({ open, onClose, setOpen, email, setEmail }) => {
+const RegisterAccount = ({
+  open,
+  onClose,
+  setOpen,
+  email,
+  setEmail,
+  pending,
+  setPending,
+  errorMsg,
+  setErrorMsg,
+}) => {
   const handleCreateAccount = (e) => {
     e.preventDefault()
 
     const checkUserExists = async (email) => {
+      setPending(true)
       try {
-        // Make an API call to check if the user exists
+        // Check if the user exists
         const response = await newRequest.get(`/users/checkUser?email=${email}`)
         const data = await response.data
 
-        if (data.invalidEmail) return alert('Invalid Email')
-        if (data.userExists) {
-          alert('User already exists')
+        if (data) setPending(false)
 
-          setOpen((prevState) => ({
-            ...prevState,
-            registerAccount: !prevState.registerAccount,
-            signIn: !prevState.signIn,
-          }))
+        if (data.invalidEmail) return setErrorMsg('Invalid Email')
+        if (data.userExists) {
+          setErrorMsg('User already exists, Login')
+
+          setTimeout(() => {
+            setOpen((prevState) => ({
+              ...prevState,
+              registerAccount: !prevState.registerAccount,
+              signIn: !prevState.signIn,
+            }))
+          }, 3000)
         } else {
           // Proceed with the next steps of registration
           setOpen((prevState) => ({
@@ -34,7 +49,10 @@ const RegisterAccount = ({ open, onClose, setOpen, email, setEmail }) => {
           }))
         }
       } catch (error) {
+        setPending(false)
+        setErrorMsg('Error checking user existence')
         console.error('Error checking user existence:', error)
+        return
       }
     }
 
@@ -64,7 +82,7 @@ const RegisterAccount = ({ open, onClose, setOpen, email, setEmail }) => {
   }
   return (
     <>
-      <Modal open={open} onClose={onClose}>
+      <Modal open={open} onClose={onClose} pending={pending}>
         <div className='reg-user df fdc'>
           <h3>Create an account</h3>
           <button className='btn btn-tr dfacjc field-pad'>
@@ -80,21 +98,23 @@ const RegisterAccount = ({ open, onClose, setOpen, email, setEmail }) => {
             <span className='or'>Or</span>
             <hr className='line' />
           </div>
-          <input
-            type='email'
-            placeholder='Email'
-            className='input'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            onKeyPress={handleKeyPress}
-          />
-
-          <button
-            className='btn btn-mv dfacjc field-pad'
-            onClick={handleCreateAccount}
-          >
-            <span className='btn-text'>Continue with Email</span>
-          </button>
+          <form onSubmit={handleCreateAccount} className='df fdc gap-20'>
+            <input
+              type='email'
+              placeholder='Email'
+              className='input'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              onKeyPress={handleKeyPress}
+              required
+            />
+            <div className='gap-10 dfac fdc'>
+              {errorMsg && <h4 className='error-msg'>{errorMsg}</h4>}
+              <button className='btn btn-mv dfacjc field-pad'>
+                <span className='btn-text'>Continue with Email</span>
+              </button>
+            </div>
+          </form>
           <div className='form-text df fdc'>
             <p>
               By Signing up, you agree to our{' '}
