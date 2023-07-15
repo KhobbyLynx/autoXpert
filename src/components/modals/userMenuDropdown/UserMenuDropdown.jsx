0a28bae234366e54bf330c065e8e81675c4787d0
Samuel Tetteh <khobbylynx55@gmail.com>
2023-07-15 03:35:58 +0000

    Update UserMenuDropdown.jsx

--- src/components/modals/userMenuDropdown/UserMenuDropdown.jsx
+++ src/components/modals/userMenuDropdown/UserMenuDropdown.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { dropdown } from '../../../data'
 import './UserMenuDropdown.scss'
+import newRequest from '../../../utils/newRequest'
+import SpinnerLoader from '../../loader/SpinnerLoader'
 
-const UserMenuDropdown = () => {
+const UserMenuDropdown = ({ setErrorMsg, setPending, errorMsg, pending }) => {
+  const navigate = useNavigate()
   const linkToRouteMap = {
     'my account': '.',
     orders: '/orders',
@@ -22,18 +25,54 @@ const UserMenuDropdown = () => {
     </Link>
   ))
 
+  const handleLogout = async () => {
+    setPending(true)
+    try {
+      const response = await newRequest.post('/users/logout')
+      const data = await response.data
+      console.log(data)
+      if (data) {
+        setPending(false)
+        localStorage.removeItem('currentUser')
+        setPending(false)
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+      setPending(false)
+      const statusCode = error.response.status
+      if (statusCode === 400) {
+        setErrorMsg(error.response.data.message)
+      } else if (statusCode === 5000) {
+        setErrorMsg(error.response.statusText)
+      } else {
+        setErrorMsg('something went wrong')
+      }
+      return
+    }
+  }
+
   return (
-    <div className='menu-container'>
-      <div className='menu-drop'>
-        <h3 className='dfac fdc'>
-          autoXpert
-          <br />
-          <span>Welcome</span>
-        </h3>
-        <ul>{dropdownList}</ul>
-        <button className='logout-btn btn btn-mv'>Logout</button>
+    <>
+      {pending && <SpinnerLoader />}
+
+      <div className='menu-container'>
+        <div className='menu-drop'>
+          <h3 className='dfac fdc'>
+            autoXpert
+            <br />
+            <span>Welcome</span>
+          </h3>
+          <ul>{dropdownList}</ul>
+          <div>
+            {errorMsg && <span className='error-msg'>{errorMsg}</span>}
+            <button className='logout-btn btn btn-mv' onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
